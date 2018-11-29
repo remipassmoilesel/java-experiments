848fc41564878b794e04b87822ae0db23ee1f479
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-11-29 09:57:23 +0100

    Add test case

---
+++ jvm-languages/scala-starter/src/test/scala/org/remipassmoilesel/ScalaTestSpec.scala
@@ -0,0 +1,31 @@
+package org.remipassmoilesel
+
+import org.scalatest._
+
+/**
+  * Scalatest offre plusieurs styles de classes de test.
+  * - FunSuite ressemble à JUnit,
+  * - FunSpec ressemble à Mocha avec describe() et it(),
+  *
+  * http://www.scalatest.org/user_guide/selecting_a_style
+  */
+
+class FunSuiteSpec extends FunSuite {
+
+  test("Test description") {
+    assert(true)
+  }
+
+}
+
+class FunSpecSpec extends FunSpec {
+
+  describe("Test group description") {
+    it("Test description") {
+      assert(true)
+    }
+  }
+
+}
+
+
