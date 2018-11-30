233a320b76a153715b19de56e769a5f03e860191
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2018-11-30 09:30:54 +0100

    Add experiments

--- jvm-languages/scala-starter/src/main/scala/org/remipassmoilesel/person/PersonDao.scala
+++ jvm-languages/scala-starter/src/main/scala/org/remipassmoilesel/person/PersonDao.scala
@@ -4,11 +4,11 @@ import java.util.concurrent.Executors
 
 import org.remipassmoilesel.Utils
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 class PersonDao {
 
-  implicit val execContext = instantiateExecutionContext()
+  implicit val execContext = instantiateDedicatedExecutionContext()
 
   def getNames: List[Person] = {
     Utils.randomSleep()
@@ -30,24 +30,44 @@ class PersonDao {
     )
   }
 
-  def getDoubledNamesAsyncFuture(number: Integer) = {
-    for {
-      f1Res <- getNamesAsyncFuture(number)
-      f2Res <- getNamesAsyncFuture(number)
-    } yield (f1Res ++ f2Res)
-  }
-
-  def getNamesAsyncFuture(number: Integer) = {
+  def getNamesAsyncFuture(number: Integer): Future[List[Person]] = {
     Future {
       val persons = getNames
       if (number > persons.size) {
-        throw new RuntimeException(s"Invalid number of persons: $number")
+        throw new IllegalArgumentException(s"Invalid number of persons: $number")
       }
       persons.slice(0, number)
     }
   }
 
-  private def instantiateExecutionContext(): ExecutionContext = {
+  def getDoubledNamesAsyncFuture(number: Integer): Future[List[Person]] = {
+    for {
+      f1Res <- getNamesAsyncFuture(number)
+      f2Res <- getNamesAsyncFuture(number)
+    } yield f1Res ++ f2Res
+  }
+
+  def getNamesAsyncPromise(number: Integer): Future[List[Person]] = {
+    val promise = Promise[List[Person]]()
+    Future {
+      val persons = getNames
+      if (number > persons.size) {
+        promise.failure(new IllegalArgumentException(s"Invalid number of persons: $number"))
+      } else {
+        promise.success(persons.slice(0, number))
+      }
+    }
+    promise.future
+  }
+
+  def getDoubledNamesAsyncPromiseWithError(number: Integer): Future[List[Person]] = {
+    for {
+      f1Res <- getNamesAsyncPromise(number + 20)
+      f2Res <- getNamesAsyncPromise(number)
+    } yield f1Res ++ f2Res
+  }
+
+  private def instantiateDedicatedExecutionContext(): ExecutionContext = {
     ExecutionContext.fromExecutor(Executors.newFixedThreadPool(Runtime.getRuntime.availableProcessors))
   }
 
---
+++ jvm-languages/scala-starter/src/test/scala/org/remipassmoilesel/FuturesSpec.scala
@@ -0,0 +1,55 @@
+package org.remipassmoilesel
+
+import org.remipassmoilesel.person.{Person, PersonDao}
+import org.scalatest.{AsyncFunSpec, Matchers}
+
+import scala.concurrent.Future
+
+class FuturesSpec extends AsyncFunSpec with Matchers {
+
+  val db = new PersonDao()
+
+  describe("Futures and promises experiments") {
+
+    describe("Futures experiments") {
+
+      it("getNames() should return a list of persons") {
+        val persons: List[Person] = db.getNames
+        persons.size should equal(14)
+      }
+
+      it("getNamesAsyncFuture() should return a list of 3 persons") {
+        val persons: Future[List[Person]] = db.getNamesAsyncFuture(3)
+        persons.map(_.size should equal(3))
+      }
+
+      it("getNamesAsyncFuture() should throw if number of persons is too high") {
+        recoverToSucceededIf[IllegalArgumentException] {
+          db.getNamesAsyncFuture(25)
+        }
+      }
+
+      it("getDoubledNamesAsyncFuture() should return a list of 20 persons") {
+        val persons: Future[List[Person]] = db.getDoubledNamesAsyncFuture(10)
+        persons.map(_.size should equal(20))
+      }
+
+    }
+
+    describe("Promises experiments") {
+
+      it("getNamesAsyncPromise() should return a list of 3 persons") {
+        val persons: Future[List[Person]] = db.getNamesAsyncPromise(3)
+        persons.map(_.size should equal(3))
+      }
+
+      it("getDoubledNamesAsyncPromiseWithError() should throw") {
+        recoverToSucceededIf[IllegalArgumentException] {
+          db.getDoubledNamesAsyncPromiseWithError(10)
+        }
+      }
+
+    }
+  }
+
+}
