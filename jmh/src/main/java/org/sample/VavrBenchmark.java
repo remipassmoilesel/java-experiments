d7751f43758985f1bc67e7cf7da93c6432e09fa1
Rémi Passmoilesel <r.passmoilesel@protonmail.com>
2019-03-11 23:17:06 +0100

    Add jmh benchmark

--- jmh/src/main/java/org/sample/VavrBenchmark.java
+++ jmh/src/main/java/org/sample/VavrBenchmark.java
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.sample;
 
@@ -44,10 +14,12 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 public class VavrBenchmark {
 
     public static final int ITEMS = 5000;
+    public static final int ITERATIONS = 5;
+    public static final int TIME = 1000;
 
     @Benchmark
-    @Warmup(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
-    @Measurement(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
+    @Warmup(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    @Measurement(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
     public void arrays() {
 
         int[] items = new int[ITEMS];
@@ -62,8 +34,8 @@ public class VavrBenchmark {
     }
 
     @Benchmark
-    @Warmup(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
-    @Measurement(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
+    @Warmup(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    @Measurement(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
     public void lists() {
 
         ArrayList<Integer> items = new ArrayList<>();
@@ -78,8 +50,8 @@ public class VavrBenchmark {
     }
 
     @Benchmark
-    @Warmup(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
-    @Measurement(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
+    @Warmup(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    @Measurement(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
     public void vavrLists() {
 
         List<Integer> list = Stream.from(0).take(ITEMS).map(i -> i).toList();
@@ -91,14 +63,27 @@ public class VavrBenchmark {
     }
 
     @Benchmark
-    @Warmup(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
-    @Measurement(iterations = 20, time = 1000, timeUnit = MILLISECONDS)
+    @Warmup(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    @Measurement(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
     public void vavrListToJavaList() {
 
         List<Integer> list = Stream.from(0).take(ITEMS).map(i -> i).toList();
 
         for (int i = 0; i < ITEMS; i++) {
             java.util.List<Integer> copy = list.toJavaList();
+        }
+
+    }
+
+    @Benchmark
+    @Warmup(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    @Measurement(iterations = ITERATIONS, time = TIME, timeUnit = MILLISECONDS)
+    public void vavrListAsJava() {
+
+        List<Integer> list = Stream.from(0).take(ITEMS).map(i -> i).toList();
+
+        for (int i = 0; i < ITEMS; i++) {
+            java.util.List<Integer> copy = list.asJava();
         }
 
     }
